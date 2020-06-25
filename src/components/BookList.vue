da992484f8dcfa759ce68b1e779ec15e0d70ae51
Anwar <anyes202@hotmail.com>
2020-06-26 00:59:16 +0300

    task 2-1

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -3,26 +3,26 @@
 <h1> {{title}}</h1>
 <ul>
 <li v-for= " book in Books">{{book.title}}:{{book.author}}</li>
-
 </ul>
-
 </div>
 </template>
+
 <script>
 export default{
 name: 'BookList',
 data(){
 return{
 title: 'All Books'
-book:[
-{title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+books:[
+   {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
    {title: 'American Gods', author: 'Neil Gaiman'},
-   {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},]
-}
+   {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+   ]
 }
 }
 }
 </script>
+
 <style>
 h1, h2{
 font-weight:normal
