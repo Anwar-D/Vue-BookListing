8b8b90f3ded0a8030370e28aeda0cf9f76bec922
Anwar <anyes202@hotmail.com>
2020-06-26 21:02:13 +0300

    task3

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,12 +1,9 @@
 <template>
   <div>
     <h1>{{title}}</h1>
-    <ul>
-      <book-item
-        v-for='book in books'
-        :key='book.id'
-        :book='book'
-      >{{book.title}}:{{book.author}}</book-item>
+
+    <ul class="exists">
+      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
     </ul>
     <br><hr>
     <book-form @addBook='appendBook'></book-form>
@@ -14,39 +11,57 @@
 </template>
 
 <script>
-import BookItem from './BookItem';
-import BookForm from './BookForm';
+import BookItem from "./BookItem";
+import BookForm from "./BookForm";
+
 export default {
-  name: 'BookList',
+  name: "BookList",
   data() {
     return {
-      title: 'All Books',
-      states: ['Want to Read', 'Read', 'Reading'],
+      title: "All Books",
+       states: ["Want to Read", "Read", "Reading"],
       books: [
-        { title: 'Self-Reliance', author: 'Ralph Waldo Emerson' },
-        { title: 'American Gods', author: 'Neil Gaiman' },
-        { title: 'Amusing Ourselves to Death', author: 'Neil Postman' },
-      ],
-    };
-  },
-  components: {
-    BookItem,
-    BookForm,
-  },
-  methods: {
-    appendBook(bookTitle, bookAuthor) {
-      this.books.push({ title: bookTitle, author: bookAuthor });
-    },
-  },
+      {
+     title: "Self-Reliance",
+     author: "Ralph Waldo Emerson",
+     finishedReading: true
+   },
+   {
+     title: "American Gods",
+     author: "Neil Gaiman",
+     finishedReading: false
+   },
+   {
+     title: "Amusing Ourselves to Death",
+     author: "Neil Postman",
+     finishedReading: true
+   }
+ ]
+};
+},
+components: {
+BookItem,
+   BookForm
+   },
+    methods: {
+    appendBook(bookData) {
+     this.books.push({
+       title: bookData.bookTitle,
+       author: bookData.bookAuthor,
+       finishedReading: bookData.finishedReading
+     });
+   }
+ }
 };
 </script>
 
 <style>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
   list-style-type: none;
   padding: 0;
 }
-</style> 
+</style>
